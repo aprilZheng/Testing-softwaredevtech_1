3810c1fc69c294c6863cf337b3a0f6a3ec95bdcf
tonglin <zhengtonglin211@gmail.com>
2020-02-25 12:32:19 +0100

    link list

---
+++ linklist.cs
@@ -0,0 +1,22 @@
+using System;
+namespace linkedList
+{
+    interface ILinked_list<T>
+    {
+        //interface only store public messages
+        void Push(T data);
+
+        void Pop();
+
+        int GetLength();
+
+        bool InsertAfter(Node<T> targerElem, T data);
+
+        bool Remove(Node<T> removeTarget);
+
+        LinkedList<T> Clone();
+
+        bool Swap(Node<T> firstnode, Node<T> secondnode);
+    }
+}
+
